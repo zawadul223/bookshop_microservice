7ef0a851be3f24baa1c9562c61033c5b185fcec7
Zawadul Islam <zawadul.islam@bjitacademy.com>
2023-06-05 10:36:41 +0600

    Added all the services without dependencies. Only the discovery_server has two dependencies

---
+++ cloud_config/src/test/java/com/bjit/cloud_config/CloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bjit.cloud_config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
